75a8e2d327ceb177a62c0df2ba3de65bba406ef8
Calvin Pelletier <calvin.pelletier@gmail.com>
2023-06-17 10:19:56 -0700

    create task dataset

--- const.py
+++ const.py
@@ -3,6 +3,7 @@ import cu
 
 DATA_PATH = cu.get_env_path('DATA_PATH') / 'ac'
 GAME_DB_PATH = DATA_PATH / 'db/game'
+TASK_DS_PATH = DATA_PATH / 'ds/task'
 
 ELO_BINS = [1500, 1750, 2000, 2250, 2500]
 PLY_BINS = [8, 16, 32, 64]
